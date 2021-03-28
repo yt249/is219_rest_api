7d0a4e886ed9c2c91ac4a96af889008bff39de5b
yt249 <yt249@njit.edu>
2021-03-28 13:15:29 -0400

    changed to grades table

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,22 @@
+/*
+DataTable plugins for jQuery:
+    takes the table in return and maps into
+    this column
+ */
 (function ($) {
-    $('#citiesTable').DataTable( {
-        "ajax": "http://localhost:9080/api/v1/cities",
+    $('#gradesTable').DataTable( {
+        "ajax": "http://localhost:9080/api/v1/grades",
         "columns": [
             { "data": "id" },
-            { "data": "fldName" },
-            { "data": "fldLat" },
-            { "data": "fldLong" },
-            { "data": "fldCountry" },
-            { "data": "fldAbbreviation" },
-            { "data": "fldCapitalStatus" },
-            { "data": "fldPopulation" }
-
+            { "data": "Last_name" },
+            { "data": "First_name" },
+            { "data": "SSN" },
+            { "data": "Test1" },
+            { "data": "Test2" },
+            { "data": "Test3" },
+            { "data": "Test4" },
+            { "data": "Final" },
+            { "data": "Grade" }
         ]
     } );
 
--- src/server.js
+++ src/server.js
@@ -6,25 +6,26 @@ const open = require("open");
 app.use(express.static('docs'));
 open("http://localhost:9080");
 
-
-app.get('/api/v1/cities', function(req, res) {
-    const mysql = require('mysql')
+// v1: first version
+// req: request comes in
+// res: respond with json response
+app.get('/api/v1/grades', function(req, res) {
+    const mysql = require('mysql');
     const connection = mysql.createConnection({
         host: 'localhost',
         port: '32000',
         user: 'root',
         password: 'root',
-        database: 'citiesData'
+        database: 'gradesData'
     });
     connection.connect();
 
-    connection.query('SELECT * FROM tblCitiesImport', function (err, rows, fields) {
+    connection.query('SELECT * FROM grades', function (err, rows, fields) {
         if (err) throw err;
         res.json({ "data": rows});
-    })
+    });
 
     connection.end()
-
 
 });
 
